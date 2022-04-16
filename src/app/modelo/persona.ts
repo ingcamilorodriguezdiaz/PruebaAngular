e459ac2773d6e27cc21c19205ef2a4f7dfb1c795
ingcamilorodriguezdiaz <ing.camilorodriguezdiaz@gmail.com>
2022-04-15 20:29:41 -0500

    Squashed commit of the following:
    commit bb043c0a827bdf5535d715f19cec991417caeb9e
    Author: ingcamilorodriguezdiaz <ing.camilorodriguezdiaz@gmail.com>
    Date:   Fri Apr 15 20:09:27 2022 -0500
    
        agregar cedula al model persona
    
    commit 95e09214171a80aa574deea574db97845fa39733
    Merge: 5e6ec30 e914201
    Author: ingcamilorodriguezdiaz <ing.camilorodriguezdiaz@gmail.com>
    Date:   Fri Apr 15 15:16:22 2022 -0500
    
        fusion de cambios
    
    commit 5e6ec300990924aee76f2fed0f8bff9363ee775c
    Author: ingcamilorodriguezdiaz <ing.camilorodriguezdiaz@gmail.com>
    Date:   Fri Apr 15 15:11:43 2022 -0500
    
        se agrega apellido en el modelo persona

--- src/app/modelo/persona.ts
+++ src/app/modelo/persona.ts
@@ -3,8 +3,10 @@ export class Persona {
   nombre: string|undefined;
   estado:number|undefined;
   tipo:string | undefined;
+  apellido:string|undefined;
   updated_at: Date|undefined;
   created_At: Date | undefined;
+  cedula:string |undefined;
   constructor(){
 
   }
